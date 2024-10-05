f9b5d9a1c7600a520c05001048e445ca3e6e4001
Andrew Kaminer <akaminer@gatech.edu>
2024-10-05 18:38:04 -0400

    update description

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Cell CV Package
+Computer Vision Package for Microfluidics. For more information, contact akaminer@gatech.edu.
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as f:
+    description = f.read()
+
 setup(
     name="golgi-cell-cv",
-    version="0.4",
+    version="0.5",
     packages=find_packages(where="src"),
     package_dir= {"": "src"},
     package_data= {"": ["*/.gitkeep"]},
-    description="description",
+    description=description,
     url="https://github.com/AndKaminer/golgi",
     author="Andrew Kaminer",
     author_email="akaminer@gatech.edu",
