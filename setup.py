99fe0e62af6cdcfd5094f7f089210adcf91e8544
Andrew Kaminer <akaminer@gatech.edu>
2025-04-03 12:48:26 -0400

    Change base folder name

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setup(
         'dash-bootstrap-components'
     ],
     entry_points={
-        'console_scripts': ["golgi-app = golgi.app:main",
-                            "golgi-set-settings = golgi.settings:set_new_settings_file",
-                            "golgi-get-settings-path = golgi.settings:print_settings_file_path",
-                            "golgi-get-setting = golgi.settings:print_setting"]}
+        'console_scripts': ["golgi-app = partivision.app:main",
+                            "golgi-set-settings = partivision.settings:set_new_settings_file",
+                            "golgi-get-settings-path = partivision.settings:print_settings_file_path",
+                            "golgi-get-setting = partivision.settings:print_setting"]}
 )
